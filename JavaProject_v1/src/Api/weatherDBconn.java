dc2c8fccf1ec90a63bffa8e7b877c2bb9daeb716
HMJda <hmj4669@gmail.com>
2022-12-01 18:51:45 +0900

    최고/최저기온 추가

--- JavaProject_v1/src/Api/weatherDBconn.java
+++ JavaProject_v1/src/Api/weatherDBconn.java
@@ -3,7 +3,15 @@ package Api;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+
+import java.time.LocalDate; //날짜
+import java.time.format.DateTimeFormatter; //시간 포맷
+import java.time.LocalTime; //시간
+
+import Api.weatherApi;
 
 public class weatherDBconn {
 	/** db 연결 */
@@ -30,33 +38,58 @@ public class weatherDBconn {
             return null;
         }
 	}
-	/** 테이블 선택해서 가져오는 부분 */
-	 public PreparedStatement select(String Table) { //테이블 선택해서 가져오기
-		 Connection connect = null;
-	     try {
-	     	Class.forName("oracle.jdbc.driver.OracleDriver");
-	         String user = "system"; 
-	         String pw = "1";
-	         String DBURL = "jdbc:oracle:thin:@localhost:1521:xe";
-	         
-	         Class.forName("oracle.jdbc.driver.OracleDriver");        
-	         connect = DriverManager.getConnection(DBURL, user, pw);          
-	     } catch (ClassNotFoundException cnfe) {
-	         System.out.println("DB 드라이버 로딩 실패 :"+cnfe.toString());
-	     } catch (SQLException sqle) {
-	         System.out.println("DB 접속실패 : "+sqle.toString());
-	     } catch (Exception e) {
-	         System.out.println("Unkonwn error");
-	         e.printStackTrace();
-	     }
+	/** 테이블 선택해서 가져오는 부분 
+	 * @throws SQLException */
+	 public ResultSet select(String Table) throws SQLException { //테이블 선택해서 가져오기
+		 Connection connect = dbConn(); 
+		 Statement stmt;
+		 stmt = connect.createStatement();
 	     String selectQuery = 
 	    			"select * from "+ Table; //테이블 선택 
 	     try {
-			PreparedStatement select = connect.prepareStatement(selectQuery);
+	    	 ResultSet select = stmt.executeQuery(selectQuery);
 			return select;
 		 } catch (SQLException e) {
 			e.printStackTrace();
 			return null;
 		 }
-	 }	
+	 }
+	 public double BringTMX() throws SQLException {
+		 Api.weatherApi w = new Api.weatherApi();
+		 double TMX = 0.0;		 
+		 Connection connect = dbConn(); 
+		 ResultSet rs = select("날씨");
+		 
+		 while(rs.next()) {	
+			 if(rs.getString(1).trim().equals(w.localDate())   ) {
+				 if(TMX==0.0) {
+					 TMX = Double.parseDouble(rs.getString(3).replaceAll(" ℃", ""));
+				 }
+				 else if(TMX < Double.parseDouble(rs.getString(3).replaceAll(" ℃", ""))) {
+					 TMX = Double.parseDouble(rs.getString(3).replaceAll(" ℃", ""));
+				 }
+			 }
+		 }		 
+		 connect.close();
+		 return TMX;
+	 }
+	 public double BringTMN() throws SQLException {
+		 Api.weatherApi w = new Api.weatherApi();
+		 double TMN = 0.0;		 
+		 Connection connect = dbConn(); 
+		 ResultSet rs = select("날씨");
+		 while(rs.next()) {
+			 if(rs.getString(1).trim().equals(w.localDate())  ) {
+				 
+				 if(TMN==0.0) {
+					 TMN = Double.parseDouble(rs.getString(3).replaceAll(" ℃", ""));
+				 }
+				 else if(TMN > Double.parseDouble(rs.getString(3).replaceAll(" ℃", "")) ) {					
+					 TMN = Double.parseDouble(rs.getString(3).replaceAll(" ℃", ""));
+				 }
+			 }
+		 }
+		 connect.close();
+		 return TMN;
+	 }
 }
