cc95edb8abab4eb4449a52825adf2a07dc0aa114
HMJda <hmj4669@gmail.com>
2022-12-02 12:23:16 +0900

    미사용 부분 제거

--- JavaProject_v1/src/Api/run.java
+++ JavaProject_v1/src/Api/run.java
@@ -3,13 +3,11 @@ package Api;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+
 import java.sql.Connection;//SQL 
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
-import Api.translateXY.*;
-import Api.gpsApi.*;
 
 public class run {
 	public static void main(String[] args) throws IOException, SQLException{
