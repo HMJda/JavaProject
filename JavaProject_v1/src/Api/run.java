01142eb68c7ea482e7c737102d503ce25c91bbd9
HMJda <hmj4669@gmail.com>
2022-12-02 12:01:22 +0900

    db상으로 위치정보 가져오기 구현

--- JavaProject_v1/src/Api/run.java
+++ JavaProject_v1/src/Api/run.java
@@ -1,8 +1,8 @@
 package Api;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-
-
+import java.io.InputStreamReader;
 import java.sql.Connection;//SQL 
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -22,19 +22,26 @@ public class run {
 		} catch (SQLException e2) {
 			e2.printStackTrace();
 		}	
+
 		
-		translateXY XY = new translateXY(); //translateXY 객체 생성
 		weatherApi wapi = new weatherApi();//weatherApi 객체 생성
+		/*
+		translateXY XY = new translateXY(); //translateXY 객체 생성
 		gpsApi gapi = new gpsApi(); //gpsApi 객체 생성
 		Adress gpsxy;
+		*/
+		//gpsxy = gapi.bringGpsfromApi();
 		
-		gpsxy = gapi.bringGpsfromApi();
-		
-		LatXLngY xy;
+		//LatXLngY xy;
 		/**위도 경도 입력 받아 xy좌표로 변경하는 부분*/
-		xy = XY.convertGRID_GPS(gpsxy.Y ,gpsxy.X); 
+		//xy = XY.convertGRID_GPS(gpsxy.Y ,gpsxy.X); 
     	//System.out.println(gpsxy.X+" "+gpsxy.Y); //GPS 테스트
-
+		
+		/**DB상에 위치정보 넣는 곳*/
+		System.out.println("주소를 입력해주세요 : ");
+		BufferedReader io = new BufferedReader(new InputStreamReader(System.in));
+		String address = io.readLine();
+	    dbConn.Inputxy(address);
 		
 		String base_time = "2300"; //임시 데이터 값 
 		//String PageNo = "1";
@@ -56,26 +63,26 @@ public class run {
 		
     	System.out.println("현재시각"+base_date+wapi.localTime());
     	
-    	String nx = "98"; //임시 데이터 값 부산
-    	String ny = "76";//임시 데이터 값  
+    	//String nx = "98"; //임시 데이터 값 부산
+    	//String ny = "76";//임시 데이터 값  
     	
-    	nx = Integer.toString((int)xy.x); /**변경된 위도경도 x,y값을 스트링형으로 변형*/
-    	ny = Integer.toString((int)xy.y);
+    	//nx = Integer.toString((int)xy.x); /**변경된 위도경도 x,y값을 스트링형으로 변형*/
+    	//ny = Integer.toString((int)xy.y);
     	
     	//System.out.println(nx+ny);//x,y 좌표 테스트
     	
     	for(int i = 1;i<4;i++) {
-    		wapi.bringWeaterFromApi(Integer.toString(i),base_date,base_time,nx,ny);   		
+    		wapi.bringWeaterFromApi(Integer.toString(i),base_date,base_time,dbConn.BringX().trim(),dbConn.BringY().trim());   		
     	}
-        System.out.println(gpsxy.adress); //adress 출력 
+        //System.out.println(gpsxy.adress); //adress 출력 
         //System.out.println(gpsxy.jibunAddress); //jibunAddress 출력
         
-        /*//최저 최고 기온 테스트
+        //최저 최고 기온 테스트
     	double TMX = dbConn.BringTMX();   	
     	double TMN = dbConn.BringTMN();
     	System.out.println("최고 기온 " +TMX);
     	System.out.println("최저 기온 " +TMN);
-    	*/
+    	
 	}
 
 }
