e3ceab77014b151f7033e54670317216ed51fa8d
HMJda <hmj4669@gmail.com>
2022-12-05 21:04:56 +0900

    .

--- JavaProject_v1/src/Api/run.java
+++ JavaProject_v1/src/Api/run.java
@@ -1,8 +1,8 @@
 package Api;
 
-import java.io.BufferedReader;
+//import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
+//import java.io.InputStreamReader;
 
 import java.sql.Connection;//SQL 
 import java.sql.PreparedStatement;
